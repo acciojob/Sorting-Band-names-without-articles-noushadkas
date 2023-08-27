0998889388032ee5e281e12cddfa682d4cc663cf
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-08-28 01:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+const bandNames = [
+  'The Rolling Stones',
+  'Led Zeppelin',
+  'Aerosmith',
+  'The Beatles',
+  'Queen',
+  'Pink Floyd',
+  'The Who'
+];
+
+function stripArticle(name) {
+  return name.replace(/^(a|an|the)\s+/i, '').trim();
+}
+
+const sortedBands = bandNames.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+
+const ul = document.querySelector('#bands');
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
